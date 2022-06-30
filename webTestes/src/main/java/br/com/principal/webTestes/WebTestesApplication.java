c16de7ff9caf57c7722ff9a4a79413eb4bbd0604
Gust4ngl <gustavosilvaskt@outlook.com>
2022-06-30 19:15:31 -0300

    Testando requisições em um arquivo html

---
+++ webTestes/src/main/java/br/com/principal/webTestes/WebTestesApplication.java
@@ -0,0 +1,13 @@
+package br.com.principal.webTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTestesApplication.class, args);
+	}
+
+}
---
+++ webTestes/src/test/java/br/com/principal/webTestes/WebTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.principal.webTestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
