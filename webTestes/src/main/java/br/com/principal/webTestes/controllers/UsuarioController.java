4e3249d70b4184179e3288f22d2d05c698a410d9
Gust4ngl <gustavosilvaskt@outlook.com>
2022-07-06 19:33:57 -0300

    adicionando algumas telas

--- webTestes/src/main/java/br/com/principal/webTestes/controllers/UsuarioController.java
+++ webTestes/src/main/java/br/com/principal/webTestes/controllers/UsuarioController.java
@@ -2,6 +2,7 @@ package br.com.principal.webTestes.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,13 +14,17 @@ public class UsuarioController {
 	public String login() {
 		return "login";
 	}
-	@GetMapping("/logar")
-	public String logar(@RequestParam(name = "usuario") String usuario,@RequestParam(name = "senha") String senha) {
+	@GetMapping("/paginaInicial")
+	public String paginaInicial(@RequestParam(name = "usuario") String usuario,@RequestParam(name = "senha") String senha) {
 		if(usuario.equals("g") && senha.equals("1")) {
 			return "teste/sucesso";
 		} else {
 			return "teste/falha";
 		}
+	}
+	@RequestMapping("/registrar")
+	public String registrar() {
+		return "registrar";
 	}
 	
 }
